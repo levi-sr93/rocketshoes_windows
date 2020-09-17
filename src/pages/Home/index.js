e8d05297637b58edaf1a5a54d869994b94336edb
Levi <levi-sr@outlook.com>
2020-09-17 19:11:09 -0300

    Searching products in API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,86 +1,51 @@
-import React from 'react'
-import {MdAddShoppingCart} from 'react-icons/md'
-import tenis from '../../assets/images/tenis.jpg'
-import {ProductList} from './styles';
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import {formatPrice} from '../../util/format';
 
-export default function Home(){
-    return (
-        <ProductList>
-            <li>
-                <img src={tenis}
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$ 129,90</span>
+import api from '../../services/api';
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff"/> 3
-                    </div>
+import { ProductList } from './styles';
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src={tenis}
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$ 129,90</span>
+export default class Home extends Component {
+    state = {
+        products: [],
+    }
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff"/> 3
-                    </div>
+    async componentDidMount() {
+        const response = await api.get('products');
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src={tenis}
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$ 129,90</span>
+        // formatando o preço assim que ele retorna informações da api
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff"/> 3
-                    </div>
+        this.setState({ products: data });
+    }
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src={tenis}
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$ 129,90</span>
+    render() {
+        const { products } = this.state;
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff"/> 3
-                    </div>
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image}
+                            alt={product.title}
+                        />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src={tenis}
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$ 129,90</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                        </div>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff"/> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    )
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        )
+    }
 }
