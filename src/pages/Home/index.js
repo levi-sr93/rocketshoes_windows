294d3ccdbe11f86ed253ab9d66e78174e4ddb7b2
Levi <levi-sr@outlook.com>
2020-09-17 00:07:37 -0300

    Added the Home Styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,86 @@
-import React from 'react';
+import React from 'react'
+import {MdAddShoppingCart} from 'react-icons/md'
+import tenis from '../../assets/images/tenis.jpg'
+import {ProductList} from './styles';
 
-export default function Home() {
-    return <h1>Home</h1>
+export default function Home(){
+    return (
+        <ProductList>
+            <li>
+                <img src={tenis}
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff"/> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src={tenis}
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff"/> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src={tenis}
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff"/> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src={tenis}
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff"/> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src={tenis}
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff"/> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    )
 }
