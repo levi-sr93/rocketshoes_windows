dddc2553b0a65c5b402141c8c568fdab55fbae28
Levi <levi-sr@outlook.com>
2020-09-17 21:48:01 -0300

    Deleting products from cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,14 +1,14 @@
 import produce from 'immer'
 
-export default function cart(state=[], action){
+export default function cart(state = [], action) {
 
 
-    switch(action.type) {
+    switch (action.type) {
         case 'ADD_TO_CART':
             return produce(state, draft => {
                 const productIndex = draft.findIndex(p => p.id === action.product.id)
 
-                if(productIndex >= 0) {
+                if (productIndex >= 0) {
                     draft[productIndex].amount += 1;
                 } else {
                     draft.push({
@@ -16,7 +16,15 @@ export default function cart(state=[], action){
                         amount: 1,
                     })
                 }
-            })
+            });
+        case 'REMOVE_FROM_CART':
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(p => p.id === action.id)
+
+                if (productIndex >= 0) {
+                    draft.splice(productIndex, 1);
+                }
+            });
         default:
             return state;
     }
